1c6d4f9f6b27b8a0059ca8ec5296fa53f2a63acd
declannelson <dfnelson178@gmail.com>
2022-07-16 07:12:43 -0400

    ignore hidden api keys, added bookAI we used to scrape new novels

--- bookAI.py
+++ bookAI.py
@@ -11,6 +11,8 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
+import math
+
 cred = credentials.Certificate("firebase_admin_key.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
@@ -85,7 +87,6 @@ def get_characters(doc):
     # return the most significant characters
     return character_occurrences.head(max_diff_idx)
 
-
 def get_character_sentences(character, doc):
     """Get list of all sentences directly or indirectly referencing a character."""
     character_sentences = []
@@ -127,20 +128,21 @@ def get_most_and_least_positive_sentences(doc, character_analysis):
     return list(most_positive_sentences), list(least_positive_sentences)
 
 
-def get_character_analysis(character, doc):
+def get_character_analysis(character, doc, doc_id):
     character_sentences = get_character_sentences(character, doc)
-    analysis = [(idx, sentence._.blob.polarity) for idx, sentence in character_sentences]
+    analysis = [(idx, sentence._.blob.polarity, doc_id) for idx, sentence in character_sentences]
     return analysis
 
 
-def get_book_analysis(window, doc):
-    sents = list(doc.sents)
+def get_book_analysis(window, docs):
     scores = []
+    for doc in docs:
+        sents = list(doc.sents)
 
-    for i in tqdm(range(len(sents) - window)):
-        text = sents[i: i + window]
-        text_score = sum([sent._.blob.polarity for sent in text]) / window
-        scores.append(text_score)
+        for i in tqdm(range(len(sents) - window)):
+            text = sents[i: i + window]
+            text_score = sum([sent._.blob.polarity for sent in text]) / window
+            scores.append(text_score)
 
     return scores
 
@@ -151,35 +153,56 @@ def main(title, save_to_db=True):
 
     # load book into nlp analyzer
     if len(book) > 1e6:
-        print('too large')
-        print(len(book))
-        return
-    doc = nlp(book)
-    number_of_sentences = len(list(doc.sents))
-    number_of_words = len(list(doc))
+        words = book.split()
+        book_sections = np.array_split(words, math.ceil(len(book) / 1e6))
+        book_sections = [' '.join(words) for words in book_sections]
+    else:
+        book_sections = [book]
+
+    docs = []
+    for section in tqdm(book_sections):
+        docs.append(nlp(section))
+
+    number_of_sentences = sum([len(list(doc.sents)) for doc in docs])
+    number_of_words = sum([len(list(doc)) for doc in docs])
 
     # get most popular characters in novel
-    character_occurrences = get_characters(doc)
+    character_occurrences = [get_characters(doc) for doc in docs]
+
+    d1 = dict(character_occurrences[0])
+    for i in range(1, len(character_occurrences)):
+        d1.update(dict(character_occurrences[i]))
+    character_occurrences = d1
+
     characters = list(character_occurrences.keys())
 
-    characters_analysis = {character: get_character_analysis(character, doc) for character in characters}
+    all_characters_analysis = {}
+    for doc_id, doc in enumerate(docs):
+        characters_analysis = {character: get_character_analysis(character, doc, doc_id) for character in characters}
+        for character, analysis in characters_analysis.items():
+            if character in all_characters_analysis:
+                all_characters_analysis[character].extend(analysis)
+            else:
+                all_characters_analysis[character] = analysis
+
     average_characters_polarity = {
-        character: sum([score for _, score in analysis]) / len(analysis)
-        for character, analysis in characters_analysis.items()
+        character: sum([score for _, score, _ in analysis]) / len(analysis)
+        for character, analysis in all_characters_analysis.items()
     }
 
     book_analysis_window = 50
-    book_analysis = get_book_analysis(book_analysis_window, doc)
+    book_analysis = get_book_analysis(book_analysis_window, docs)
 
     characters_json = []
     for character, occurrence_count in character_occurrences.items():
         characters_json.append({
             'name': character,
-            'occurrence_count': occurrence_count,
+            'occurrence_count': int(occurrence_count),
             'avg_polarity': average_characters_polarity[character],
-            'sentiment_analysis_scores': [score for _, score in characters_analysis[character]],
-            'sentiment_analysis_indices': [idx for idx, _ in characters_analysis[character]],
-            'sentiment_analysis_sentences': [doc[idx].sent.text for idx, _ in characters_analysis[character]],
+            'sentiment_analysis_scores': [score for _, score, _ in characters_analysis[character]],
+            'sentiment_analysis_indices': [idx for idx, _, _ in characters_analysis[character]],
+            'sentiment_analysis_sentences': [docs[doc_idx][token_idx].sent.text for token_idx, _, doc_idx in
+                                             characters_analysis[character]],
         })
 
     if save_to_db:
@@ -195,6 +218,31 @@ def main(title, save_to_db=True):
 
 
 if __name__ == "__main__":
-    titles = ['Heart of Darkness', 'The Great Gatsby']
-    for title in titles:
-        main(title, save_to_db=True)
+    popular_titles = [
+        'Moby Dick; Or, The Whale',
+        'Pride and Prejudice',
+        'Alice\'s Adventures in Wonderland',
+        'The Adventures of Sherlock Holmes',
+        'Frankenstein; Or, The Modern Prometheus',
+        'Dracula',
+        'Ulysses',
+        'The Yellow Wallpaper',
+        'The Picture of Dorian Grey',
+        'A Tale of Two Cities',
+        'Birds from North Borneo',
+        'A Modest Proposal',
+        'Japanese Girls and Women',
+        'The Count of Monte Christo',
+        'War and Peace',
+        'Crime and Punishment',
+        'The Iliad',
+        'Adventures of Huckleberry Finn',
+        'The Odyssey'
+    ]
+    for title in popular_titles:
+        try:
+            print(f'processing: {title}...')
+            main(title, save_to_db=True)
+        except Exception:
+            print('ERROR')
+            print(Exception)
